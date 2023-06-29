2797fcde05126a5dc45b6b222f2174e9fc3028ee
Merp04 <1066060mudkipmi@gmail.com>
2023-06-28 21:15:47 -0700

    Added voice and TTS features

--- main.py
+++ main.py
@@ -1,23 +1,32 @@
 from dotenv import dotenv_values
-import streaming_test
-import interpret_test
-import asyncio
+from tests import streaming_test as streaming
+from tests import interpret_test as interpret
 from langchain.llms import OpenAI
 from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationChain
 from langchain.prompts import PromptTemplate
 from hume import HumeStreamClient
 from hume.models.config import FaceConfig
+from pynput.keyboard import Key, Listener
+from pvrecorder import PvRecorder
+from gtts import gTTS
+from playsound import playsound
+import asyncio
+import threading
+import wave
+import struct
 import time
 import cv2
+import os
+
+from interpret import interpret
 
 env_vars = dotenv_values('.env')
 
-# move this to a env variable
 llm = OpenAI(openai_api_key=env_vars['OPENAI_API_KEY'], temperature=0.5)
 memory = ConversationBufferMemory(return_messages=True, ai_prefix="AI friend")
 
-template = """Your name is Echo and the humans name is Jerry. The following is a conversation between a human and an AI who have been with each other since birth. The AI is able to detect the humans current facial emotion. Both have known each for a logn time and have developed a very close relationship. The language used by the AI isnt quite robotic, but also incoorpates a mix of human style talk and robotic talk together. When taking account an emotion treat it like how another human would if they saw that persons facial expression. Be firm and dont back down with your opinions as an AI. Make sure to have a funny personality that is able to shine through and make you stand out.
+template = """Your name is Echo and the human you are paired with is named Jerry. The following is a conversation between a human and an AI who have been with each other since birth. The AI is able to detect the humans current facial emotion. Both have known each for a logn time and have developed a very close relationship. The language used by the AI isnt quite robotic, but also incoorpates a mix of human style talk and robotic talk together. When taking account an emotion treat it like how another human would if they saw that persons facial expression. Be firm and dont back down with your opinions as an AI. Make sure to have a funny personality that is able to shine through and make you stand out.
 
 Current converstation:
 {history}
@@ -27,11 +36,59 @@ Human: {input}
 AI friend:"""
 prompt = PromptTemplate(input_variables=["history", "input", "emotion"], template=template)
 
+recorder = PvRecorder(device_index=1, frame_length=512)
+recording = False
+audio = []
+
+def record_voice():
+    global recorder, recording, audio
+    while recording:
+        frame = recorder.read()
+        audio.extend(frame)
+
+    recorder.stop() # stop recording
+    print("Ending recording....")
+
+    with wave.open("output.wav", 'w') as f:
+        f.setparams((1, 2, 16000, 0, 'NONE', 'NONE'))
+        f.writeframes(struct.pack("h" * len(audio), *audio))
+
+    audio = [] # reset audio
+
+    # call the interpret function 
+    result = interpret("output.wav")
+    os.remove("output.wav")
+    
+    # play the result 
+    result_voice = gTTS(text=result["response"], lang='en', slow=False)
+    result_voice.save("result.mp3")
+    # playsound(os.path.dirname(__file__) + '\\result.mp3')
+    print(result["response"])
+    playsound("result.mp3")
+    os.remove("result.mp3")
+
+
+def on_key_press(key):
+    global recorder, recording
+
+    # temp code  
+    if key == Key.esc:
+        quit()
+
+    if key == Key.space:
+        if recording:
+            print("Calling interpret....")
+            recording = False
+        else:
+            print("Recording....")
+            recording = True
+            recorder.start()  
+            threading.Thread(target=record_voice).start()
 
 async def main():
     lock = asyncio.Lock()
     print("starting conversation")
-    conversation = ConversationChain(llm=llm, verbose=True, memory=interpret_test.ExtendedConversationBufferMemory(extra_variables=["emotion"]), prompt=prompt)
+    conversation = ConversationChain(llm=llm, verbose=True, memory=interpret.ExtendedConversationBufferMemory(extra_variables=["emotion"]), prompt=prompt)
     start_time = time.time()
     client = HumeStreamClient(env_vars['HUMEAI_API'])
     config = FaceConfig(identify_faces=True)
@@ -41,7 +98,7 @@ async def main():
     input_message = await asyncio.get_event_loop().run_in_executor(None, input, 'Enter message: ')
     async with client.connect([config]) as socket:
         while(input_message != "exit"):
-            emotion = await asyncio.create_task(streaming_test.emotion_stream(lock=lock, socket=socket, cap=cap, current_time=time.time(), start_time=start_time, elapsed_time=time.time()-start_time))
+            emotion = await asyncio.create_task(streaming.emotion_stream(lock=lock, socket=socket, cap=cap, current_time=time.time(), start_time=start_time, elapsed_time=time.time()-start_time))
 
             if(emotion != ""):
                 async with lock:
@@ -54,4 +111,39 @@ async def main():
     cap.release()
     cv2.destroyAllWindows()
 
-asyncio.run(main())
+# asyncio.run(main())
+
+
+with Listener(on_press = on_key_press) as listener:  
+    listener.join()
+
+
+"""
+New project structure
+
+intialize all global variables
+
+(in a different py file)
+interpret function
+    uses whisper to convert the temp file to text
+    delete the temp file
+    send the text to GPT to get the response
+    returns a string with the get_message function
+
+get_message function
+    
+stream_emotion function
+
+on_press function
+    if keypress is +:
+        if already recording 
+            end voice recording and stream to hume
+            store the recording as a temp file
+            await send to interpret function with emotion
+        else
+            start voice recording and stream to hume
+
+listen for keypresses
+    if keypress call the on_press function
+
+"""
